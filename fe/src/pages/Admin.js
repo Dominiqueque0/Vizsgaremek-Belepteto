29e18b1a63ed89ef078e55adce6b14082e7285a1
Dominiqueque0 <19c_godad@nyirszikszi.hu>
2024-04-10 20:24:33 +0200

    Backend és Frontend feltöltés

---
+++ fe/src/pages/Admin.js
@@ -0,0 +1,37 @@
+import {TeljesTablaAdmin} from '../components/table/Table';
+import {BelepoTablaAdmin} from '../components/table/Table';
+import {AppBarAdmin} from '../components/navbar/Navbar';
+import { useNavigate } from 'react-router-dom';
+import {useEffect} from 'react'
+import axios from 'axios'
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------ADMIN------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+
+export default function Portas(){
+  const history = useNavigate(); 
+  useEffect(() => {
+    if(localStorage.getItem('token') === null){
+      history('/')
+    }
+    if(localStorage.getItem('auth') !== "PORTAS"){
+      history('/admin')
+    }
+  },[])
+return (<>
+<AppBarAdmin></AppBarAdmin>
+<div className="container">
+  <div className='jobbtabla'>
+    <TeljesTablaAdmin></TeljesTablaAdmin>
+  </div>
+</div>
+<div className='container2'>
+<div className='kozeptabla'>
+    <BelepoTablaAdmin className="tabla"></BelepoTablaAdmin>
+  </div>
+</div>
+</>)
+}
